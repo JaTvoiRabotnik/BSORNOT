9af7638bc5649f5cea132a4823a80966ae1dc001
Marco Zanchi <marco.zanchi@tma.com.vn>
2018-07-10 00:45:21 -0700

    making changes to evaluator to respond with score

--- bsornot/evaluator.py
+++ bsornot/evaluator.py
@@ -2,11 +2,20 @@
 import re
 
 
-def evaluate(text, username):
-    """Respond to query for a BS evaluation."""
+def evaluate(text):
+    """Return a score for BS."""
     explanation = re.compile('.+but.+', re.IGNORECASE)
     if explanation.match(text):
-        answer = " Explanation is usually BS"
+        return 0.9
     else:
-        answer = " I don't know yet if this is BS or not."
-    return '@{0}'.format(username) + answer
+        return 0.7
+
+
+def parse_evaluation(text, username):
+    """Respond to query for a BS evaluation."""
+    score = evaluate(text)
+    response = '@{0}'.format(username)
+    if score >= 0.8:
+        return response + " Explanation is usually BS"
+    else:
+        return response + " I don't know yet if this is BS or not."
